1dd81394b8e1d6d75c5b78217d18d36d64f8d877
charli <charlyoleg@fabfolk.com>
2020-01-17 00:32:31 +0100

    backend typescript config

---
+++ backend/swaggerDef.js
@@ -0,0 +1,13 @@
+// swaggerDef.js
+
+module.exports = {
+  info: {
+    // API informations (required)
+    title: 'rest_wendy_app', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'A rest-api for the wendy-app', // Description (optional)
+  },
+  host: 'https://localhost:9631', // Host (optional)
+  basePath: '/', // Base path (optional)
+};
+
